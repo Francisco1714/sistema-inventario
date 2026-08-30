62c649d3dfc5194b7e63f5cd09dff064bcc16680
Francisco1714 <francisco.sandoval.hevia@gmail.com>
2026-08-30 11:15:28 -0400

    Se agrega la tabla de productos

---
+++ database/tablas.py
@@ -0,0 +1,23 @@
+from database.conexion import obtener_conexion 
+
+# --------------------------------------------------
+# CREAR TABLA DE PRODUCTOS
+# --------------------------------------------------
+
+def crear_tabla_productos():
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    # aqui ira el create table 
+    cursor.execute(""" 
+    CREATE TABLE IF NOT EXISTS productos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT,
+        descripcion TEXT,
+        precio REAL,
+        stock INTEGER
+    )
+""")
+    
+    conexion.commit()
+    conexion.close()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from database.conexion import obtener_conexion
+from database.tablas import crear_tabla_productos
 
 conexion = obtener_conexion()
 
 print("Conexión a SQLite establecida con éxito.")
 
 conexion.close()
+
+crear_tabla_productos()
+print("Tabla de productos creada con éxito.")
