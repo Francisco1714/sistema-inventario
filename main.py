74f4a88d8e0e3bfe266f20e8b70438c467a43d22
Francisco1714 <francisco.sandoval.hevia@gmail.com>
2026-08-31 16:06:22 -0400

    refactor: separar interfaz CLI de main

--- main.py
+++ main.py
@@ -1,47 +1,5 @@
 from database.tablas import crear_tabla_productos
-from productos.crud import agregar_producto, listar_productos, modificar_producto, eliminar_producto    
-
-# --------------------------------------------------
-# MENÚ PRINCIPAL
-# --------------------------------------------------
-
-def menu():
-    while True:
-
-        print()
-        print("================================")
-        print("       SISTEMA DE INVENTARIO")
-        print("================================")
-        print()
-        print("1. Agregar producto")
-        print("2. Listar productos")
-        print("3. Modificar producto")
-        print("4. Eliminar producto")
-        print("5. Salir")
-        print()
-
-        opcion = input("Seleccione una opción: ")
-
-        match opcion:
-
-            case "1":
-                agregar_producto()
-
-            case "2":
-                listar_productos()
-
-            case "3":
-                modificar_producto()
-
-            case "4":
-                eliminar_producto()
-
-            case "5":
-                print("Saliendo del sistema...")
-                break
-
-            case _:
-                print("Opción no válida.")
+from cli.menu import menu
 
 # --------------------------------------------------
 # INICIAR APLICACIÓN
